3ab9bbfb5c77f3c89adaaf5d53c15d23dfc1784f
Jaron Warren <jarwarren@gmail.com>
2023-04-25 16:25:26 -0600

    updated tutorial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { useState, useRef, useEffect } from 'react'
 import TodoList from './TodoList'
 import { v4 as uuidv4 } from 'uuid'
+import "./style.css"
 
 function App() {
-  const [todos, setTodos] = useState<string[]>([])
+  const [todos, setTodos] = useState<any[] | null>(null)
   const todoNameRef: any = useRef()
   const LOCAL_STORAGE_KEY: string = 'todoApp.todos'
 
@@ -17,7 +18,7 @@ function App() {
   }, [todos])
 
   function toggleTodo(id: typeof uuidv4) {
-    const newTodos = [...todos]
+    const newTodos = todos ? [...todos] : []
     const todo = newTodos.find(todo => todo.id === id)
     todo.complete = !todo.complete
     setTodos(newTodos)
@@ -34,11 +35,11 @@ function App() {
 
   return (
     <>
-      <TodoList todos={todos} />
       <input ref={todoNameRef} type="text" />
-      <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed</button>
+      <button className='btn' onClick={handleAddTodo}>Add Todo</button>
+      <button className='btn'>Clear Completed</button>
       <div>{TodoList.length} left to do</div>
+      <TodoList todos={todos} />
     </>
   )
 }
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import Todo from './Todo'
 
 export default function TodoList({ todos }: any) {
+  if (!todos) {
+    todos = []
+  }
   return (
     todos.map((todo: any) => {
       return <Todo key={todo} todo={todo} />
