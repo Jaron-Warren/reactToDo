0cc7398cb80f28e738626c522308dd0c10c2999c
Jaron Warren <jarwarren@gmail.com>
2023-04-27 22:23:44 -0600

    updated tutorial code

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,52 @@
-import { useState, useRef, useEffect } from 'react'
-import TodoList from './TodoList'
-import { v4 as uuidv4 } from 'uuid'
+import { useEffect, useState } from "react"
+import { TodoForm } from "./TodoForm"
 import "./style.css"
+import { TodoList } from "./TodoList"
 
-function App() {
-  const [todos, setTodos] = useState<any[]>([])
-  const todoInput: any = useRef()
-  const LOCAL_STORAGE_KEY: string = 'todoApp.todos'
+export default function App() {
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
 
   useEffect(() => {
-    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) ?? '')
-    if (storedTodos) setTodos( prevTodos => [...prevTodos, ...storedTodos] );
-  }, [])
-
-  useEffect(() => {
-    console.log(todos)
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
   }, [todos])
 
-  function toggleTodo(id: typeof uuidv4) {
-    const newTodos = todos ? [...todos] : []
-    const todo = newTodos.find(todo => todo.id === id)
-    todo.complete = !todo.complete
-    setTodos(newTodos)
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title, completed: false },
+      ]
+    })
   }
 
-  function handleAddTodo(e: any) {
-    const name = todoInput.current.value
-    if (name === '') return
-    setTodos((prevTodos: any) => {
-      return [...prevTodos, { id: crypto.randomUUID(), name: name, complete: false }]
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
     })
-    todoInput.current.value = null
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
     <>
-      <input ref={todoInput} type="text" />
-      <button className='btn' onClick={handleAddTodo}>Add Todo</button>
-      <button className='btn'>Clear Completed</button>
-      <div>{TodoList.length} left to do</div>
-      <TodoList todos={todos} />
+      <TodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
     </>
   )
 }
-
-export default App
--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Todo from './Todo'
+import { TodoItem } from "./TodoItem"
 
-export default function TodoList({ todos }: any) {
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
   return (
-    todos.map((todo: any) => {
-      return <Todo key={todo} todo={todo} />
-    })
+    <ul className="list">
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <TodoItem
+            {...todo}
+            key={todo.id}
+            toggleTodo={toggleTodo}
+            deleteTodo={deleteTodo}
+          />
+        )
+      })}
+    </ul>
   )
 }
